910a4388edd448e10915130b4e720a8219ea6f07
Sathya Kumar <satkumar@diligent.com>
2026-06-15 07:07:29 +0530

    Initial commit

---
+++ AiChat/AiChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiChatApp.swift
+//  AiChat
+//
+//  Created by Sathya Kumar on 14/06/26.
+//
+
+import SwiftUI
+
+@main
+struct AiChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AiChatTests/AiChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  AiChatTests.swift
+//  AiChatTests
+//
+//  Created by Sathya Kumar on 14/06/26.
+//
+
+import Testing
+@testable import AiChat
+
+struct AiChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
